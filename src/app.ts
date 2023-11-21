26977f2ff55a7bbc419f697813d9d8f78e6c0164
Armaiic <bastiendandre.pro@gmail.com>
2023-11-21 16:19:30 +0100

    add dockerfile and docker compose

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('final teste445456465ege!!');
 });
 
 app.listen(port, () => {
