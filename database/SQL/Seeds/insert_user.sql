e93daa468548be9fcd2dec11f7ed9085aa81bffe
unknown <emma.broudic35@gmail.com>
2023-11-23 15:57:54 +0100

    tables de la bdd

--- database/SQL/Seeds/insert_user.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user (User_ID, User_Surname, User_First_Name, User_Nickname, User_Email, User_Password, User_Timestamp)
-VALUES
-(1, 'Dupont', 'Rébecca', 'Foxie', 'renard@mail-test.com', 'test1', 1700731841),
-(2, 'Martin', 'Aimée', 'Birdie', 'oiseau@mail-test.com', 'test2', 1700731932),
-(3, 'Bertrand', 'Marielle', 'Lioness', 'lion@mail-test.com', 'test3', 1629707441);
