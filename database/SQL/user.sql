7d5c26019bcd3c6bbf1dc08bd3cb02408fb0b773
Youn5 <etud8@yahoo.fr>
2023-11-23 14:07:06 +0100

    table user de la bdd

---
+++ database/SQL/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "user" (
+    User_ID SERIAL PRIMARY KEY,
+    User_Surname VARCHAR(50),
+    User_First_Name VARCHAR(50),
+    User_Nickname VARCHAR(50),
+    User_Email VARCHAR(100) UNIQUE,
+    User_Password VARCHAR(100),
+    User_Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
