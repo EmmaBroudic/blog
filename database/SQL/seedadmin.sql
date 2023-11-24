4b086aaf802fabd1802285f38e60aa8bf8558d57
unknown <emma.broudic35@gmail.com>
2023-11-24 11:44:35 +0100

    postgre client

--- database/SQL/seedadmin.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "admin" (Admin_Surname, Admin_First_Name, Admin_Nickname, Admin_Email, Admin_Password)
-VALUES
-('Doe', 'John', 'JD', 'john.doe@example.com', 'password123'),
-('Smith', 'Alice', 'AS', 'alice.smith@example.com', 'securepass456'),
-('Brown', 'Emma', 'EB', 'emma.brown@example.com', 'mysecretpass789');
--- database/SQL/seedarticle.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "article" (Admin_ID, Article_title, Article_text, Article_image)
-VALUES
-(1, 'Titre article 1', 'Texte article 1', 'image1.jpg'),
-(2, 'Titre article 2', 'Texte article 2', 'image2.jpg'),
-(1, 'Titre article 3', 'Texte article 3', 'image3.jpg');
