4bb94a33305681165d9f3d98d2f3772f47d73e81
Steph4477 <morelstephane4026@neuf.fr>
2023-11-27 21:14:40 +0100

    add method get for register.pug + register.css

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
+// app.ts
+
 import express, { Request, Response } from 'express';
+import * as path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('final teste445456465ege!!');
+// Définissez le dossier "views" pour les fichiers Pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Définissez le dossier "styles" pour les fichiers CSS
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+app.get('/register', (req: Request, res: Response) => {
+  // Utilisez le nom du fichier Pug sans extension
+  res.render('register', { pageTitle: 'register' });
 });
 
 app.listen(port, () => {
