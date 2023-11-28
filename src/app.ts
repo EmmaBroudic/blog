368d0bf35e569b25fafc312b7540e70ac7bb55a6
Steph4477 <morelstephane4026@neuf.fr>
2023-11-28 22:17:31 +0100

    Add component header.pug+css

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 const app = express();
 const port = 3000;
 
+<<<<<<< HEAD
 // Définissez le dossier "views" pour les fichiers Pug
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -15,6 +16,17 @@ app.use('/styles', express.static(path.join(__dirname, 'styles')));
 
 app.get('/register', (req: Request, res: Response) => {
   // Utilisez le nom du fichier Pug sans extension
+=======
+// Définit le dossier "views" pour les fichiers Pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Définit le dossier "styles" pour les fichiers CSS
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+app.get('/register', (req: Request, res: Response) => {
+  // Utilise le nom du fichier Pug sans extension
+>>>>>>> 6d70b24 (Add component header.pug+css)
   res.render('register', { pageTitle: 'register' });
 });
 
