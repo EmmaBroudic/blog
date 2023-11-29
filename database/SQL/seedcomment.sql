9dffc6f6d06ec86b8501a56e85fbefd1305569dd
unknown <emma.broudic35@gmail.com>
2023-11-29 14:18:39 +0100

    Commit des modifications locales avant pull

---
+++ database/SQL/seedcomment.sql
@@ -0,0 +1,5 @@
+INSERT INTO "comment" (Article_ID, Admin_ID, User_ID, Text_Com)
+VALUES
+(1, 1, 1, 'Ceci est un commentaire sur article 1'),
+(2, 2, 3, 'Ceci est un autre commentaire sur article 2'),
+(3, 1, 2, 'Un commentaire pour article 3');
