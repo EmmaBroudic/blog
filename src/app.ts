29b910b739fa8ff410c456d592fe8329d5137b99
BBJAVMP <julien1.feit@gmail.com>
2023-12-01 11:49:12 +0100

    add.pug & post function

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ async function insertUserData(User_Surname: string, User_First_name: string, Use
     // Exécution de la requête d'insertion
     await client.query(insertQuery, values);
 
-    console.log('Nouvel enregistrement créé avec succès');
+    console.log('Nouvel créé avec succès');
 
   } catch (error) {
     console.error('Erreur lors de l\'insertion des données :', error);
@@ -58,15 +58,30 @@ app.get('/register', (req: Request, res: Response) => {
   res.render('register', { pageTitle: 'register' });
 });
 
-app.get('/home', (req: Request, res: Response) => {
-  // Utilisez le nom du fichier Pug sans extension
-  res.render('home', { pageTitle: 'home' });
-});
+app.get('/add', (req: Request, res: Response) => {
+  console.log(req.headers)
+  res.render('add', {}) 
+} )
+
+app.post('/add', (req: Request, res: Response) => {
+  console.log("post", req.body)
+  res.render('add')
+})
+
+app.get('/article', (req: Request, res: Response) => {
+  console.log(req.headers)
+  res.render('article', {})
+})
 
 app.get('/login', (req: Request, res: Response) => {
-  // Utilisez le nom du fichier Pug sans extension
-  res.render('login', { pageTitle: 'login', error: '' });
-});
+  
+  res.render('login')
+})
+
+app.get('/movirec', (req: Request, res: Response) => {
+  
+  res.render('movirec')
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
@@ -74,7 +89,7 @@ app.listen(port, () => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/register', async (req, res) => {
+app.post('/submit-register', async (req, res) => {
   const client = new Client(dbConfig);
 
   try {
@@ -99,7 +114,7 @@ app.post('/register', async (req, res) => {
     await client.query(insertQuery, values);
 
     console.log('Nouvel enregistrement créé avec succès');
-    res.redirect('/home');
+    res.send('Nouvel enregistrement créé avec succès');
 
   } catch (error) {
     console.error('Erreur lors de l\'insertion des données :', error);
@@ -107,49 +122,5 @@ app.post('/register', async (req, res) => {
   } finally {
     // Fermeture de la connexion à la base de données
     await client.end();
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const loginClient = new Client(dbConfig); // Create a new client for login
-
-  try {
-    // Connexion au client PostgreSQL
-    await loginClient.connect();
-
-    // Données du formulaire
-    const userEmail = req.body.User_Email;
-    const userPassword = req.body.User_Password;
-    console.log('Données du formulaire :', userEmail, userPassword);
-
-    // Requête SQL pour vérifier les informations d'identification
-    const loginQuery = `
-      SELECT * 
-      FROM bloguser 
-      WHERE User_Email = $1 AND User_Password = $2
-    `;
-
-    // Paramètres à passer à la requête
-    const loginValues = [userEmail, userPassword];
-
-    // Exécution de la requête de connexion
-    const result = await loginClient.query(loginQuery, loginValues);
-
-    // Vérifier si des résultats ont été renvoyés (utilisateur authentifié)
-    if (result.rows.length > 0) {
-      console.log('Connexion réussie');
-      res.redirect('/home');
-    } else {
-      const errorMessage = 'Adresse e-mail ou mot de passe incorrect.';
-      console.log('Échec de la connexion :', errorMessage);
-      res.status(401).render('login', { pageTitle: 'login', error: errorMessage });
-    }
-  } catch (error) {
-    const errorMessage = 'Une erreur s\'est produite lors de la connexion.';
-    console.error('Erreur lors de la connexion :', error);
-    res.status(500).render('login', { pageTitle: 'login', error: errorMessage });
-  } finally {
-    // Fermeture de la connexion à la base de données
-    await loginClient.end();
   }
 });
